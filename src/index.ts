b3f08221025d5bb05b033e9d729d711322344b30
ssasongko <kokonur1020@gmail.com>
2024-09-01 20:51:48 +0700

    chore: Update package.json with prisma dependency and add prisma schema

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => `Welcome to a ${Bun.env.APP_NAME}`)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
