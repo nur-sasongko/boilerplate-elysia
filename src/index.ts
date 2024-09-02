3d95c0bc843e55e8e646cbd4d01d2423e8f5dc35
Nur Sasongko <kokonur1020@gmail.com>
2024-09-02 19:56:02 +0700

    feat: swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
 import Routes from "./routes";
 
 const app = new Elysia()
+  .use(swagger({
+    path: "/docs",
+  }))
   .get("/", () => `Welcome to a ${Bun.env.APP_NAME}`)
   .group("/api", (app) => app.use(Routes))
   .listen(Bun.env.APP_PORT as string);
